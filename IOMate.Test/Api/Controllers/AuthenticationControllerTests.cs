88918eea14f6a8b1fe79ce7993d6afc02765f08f
Gabriel Silveira <gabrielsilveirapro@gmail.com>
2025-08-18 21:10:10 -0300

    (feat) add authentication controller

---
+++ IOMate.Test/Api/Controllers/AuthenticationControllerTests.cs
@@ -0,0 +1,31 @@
+using IOMate.Api.Controllers;
+using IOMate.Application.UseCases.Authentication.Auth;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+public class AuthenticationControllerTests
+{
+    private readonly Mock<IMediator> _mediatorMock = new();
+    private readonly AuthenticationController _controller;
+
+    public AuthenticationControllerTests()
+    {
+        _controller = new AuthenticationController(_mediatorMock.Object);
+    }
+
+    [Fact]
+    public async Task Authenticate_ReturnsOk_WithToken()
+    {
+        var request = new AuthenticationRequestDto("user@email.com", "123456");
+        var response = new AuthResponseDto { Token = "jwt-token" };
+
+        _mediatorMock.Setup(m => m.Send(request, It.IsAny<CancellationToken>()))
+            .ReturnsAsync(response);
+
+        var result = await _controller.Auth(request);
+
+        var okResult = Assert.IsType<OkObjectResult>(result);
+        Assert.Equal(response, okResult.Value);
+    }
+}
