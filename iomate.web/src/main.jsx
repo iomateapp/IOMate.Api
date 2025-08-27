5fefbd702d4d94cd47f00c1396b82e63f56577a5
Erick Teixeira <erickhenriquemota@gmail.com>
2025-08-26 21:54:49 -0300

    (feat) add login page

--- iomate.web/src/main.jsx
+++ iomate.web/src/main.jsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import './index.css'
+
 import App from './App.jsx'
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import theme from './theme.js';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
